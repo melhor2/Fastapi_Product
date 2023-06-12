4bc12da300652aae96b0aaf2d8cd0e7b2e074f0a
melhor2 <jeremyrodriguez979@gmail.com>
2023-06-12 23:28:33 +0100

    main new

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 
-from config.database import engine,Base
+from config.database import engine, Base
 
 from middlewares.error_handler import Errorhandler
-from routers.movie import movie_router
-from routers.genres import genres_router
-
+from routers.products import product_router
+from routers.Supplier import supplier_router
+from routers.Supplies import supplies_router
 
 
 app = FastAPI()
@@ -15,18 +15,19 @@ app.title = "Mi app con FastAPI"
 app.version = "0.0.1"
 
 app.add_middleware(Errorhandler)
-app.include_router(movie_router)
-app.include_router(genres_router)
+app.include_router(product_router)
+app.include_router(supplier_router)
+app.include_router(supplies_router)
 
 
 Base.metadata.create_all(bind=engine)
 
 
-@app.get('/',tags=['home'],status_code=200)
+@app.get("/", tags=["home"], status_code=200)
 def message():
-    return HTMLResponse('<h1>Hello World</h1>')
+    return HTMLResponse("<h1>Hello World</h1>")
 
-@app.get('/hola',tags=['home'])
+
+@app.get("/hola", tags=["home"])
 def hola():
-    return HTMLResponse('<h1>Hola Clase</h1>')
-
+    return HTMLResponse("<h1>Hola Clase</h1>")
